f7c18064030fae1c5a7b715ef4ae40d2b92ae0c1
CMLTTN <cemaldim@gmail.com>
2023-03-08 09:47:23 +0300

    first commit

---
+++ clarus-library-starter/src/styles/Global.styles.jsx
@@ -0,0 +1,16 @@
+import {createGlobalStyle}from "styled-components";
+
+export const GlobalStyles = createGlobalStyle`
+*{
+     margin:0;
+     padding:0;
+     box-sizing:border-box;
+     text-transform:uppercase;
+     font-family:"Shantell Sans", cursive;
+}
+body{
+font-size: 1.5rem;
+background: ${({theme})=> theme.colors.mainColor};
+
+}
+`;
