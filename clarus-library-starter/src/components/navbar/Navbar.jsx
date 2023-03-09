b52df7ddeda2dad18a235897412615e2c7bed339
CMLTTN <cemaldim@gmail.com>
2023-03-09 09:18:21 +0300

    changes

--- clarus-library-starter/src/components/navbar/Navbar.jsx
+++ clarus-library-starter/src/components/navbar/Navbar.jsx
@@ -1,9 +1,18 @@
-import React from 'react'
+import React from "react";
+import { Logo, Menu, MenuLink, Nav } from "./Navbar.style";
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
-
-export default Navbar
+    <Nav justify="space-between">
+      <Logo to="/">Clarus Library</Logo>
+      <Menu>
+        <MenuLink to="/">Home</MenuLink>
+        <MenuLink to="/about">About</MenuLink>
+        <MenuLink to="/register">Register</MenuLink>
+        <MenuLink to="/login">Login</MenuLink>
+        <MenuLink to="/logout">Logout</MenuLink>
+      </Menu>
+    </Nav>
+  );
+};
+export default Navbar;
