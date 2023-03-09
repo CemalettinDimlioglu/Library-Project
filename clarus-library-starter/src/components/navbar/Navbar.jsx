b0cede59a7745ad4ca888c95da60137aff272953
CMLTTN <cemaldim@gmail.com>
2023-03-09 10:03:51 +0300

    login

--- clarus-library-starter/src/components/navbar/Navbar.jsx
+++ clarus-library-starter/src/components/navbar/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from "react";
-import { Logo, Menu, MenuLink, Nav } from "./Navbar.style";
+import React, { useState } from "react";
+import { HamburgerIcon, Logo, Menu, MenuLink, Nav } from "./Navbar.style";
+import { menuIcon } from "../../helper/iconData";
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false);
   return (
-    <Nav justify="space-between">
+    <Nav justify="space-between" wrap="wrap">
       <Logo to="/">Clarus Library</Logo>
-      <Menu>
+      <HamburgerIcon onClick={()=>setShowMenu(!showMenu)} >{menuIcon}</HamburgerIcon>
+      <Menu showMenu={showMenu} >
         <MenuLink to="/">Home</MenuLink>
         <MenuLink to="/about">About</MenuLink>
         <MenuLink to="/register">Register</MenuLink>
--- clarus-library-starter/src/pages/login/Login.jsx
+++ clarus-library-starter/src/pages/login/Login.jsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import { FormContainer, Header, LoginContainer,StyledButton,StyledForm, StyledInput } from "./Login.style";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <LoginContainer>
+      <FormContainer>
+      <Header>Login Here</Header>
+        <StyledForm>
+         <StyledInput type="text" placeholder="UserName"/> 
+         <StyledInput type="password" placeholder="Password"/> 
+         <StyledButton type="submit" >
+          Login 
+         </StyledButton>
+        </StyledForm>
+      </FormContainer>
+    </LoginContainer>
+  );
+};
 
-export default Login
+export default Login;
