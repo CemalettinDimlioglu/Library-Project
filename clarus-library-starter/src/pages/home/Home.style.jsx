99855448d53c921574bef103fa58bd52ce73d970
CMLTTN <cemaldim@gmail.com>
2023-03-10 11:16:03 +0300

    home srtle

---
+++ clarus-library-starter/src/pages/home/Home.style.jsx
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+import Flex from "../../styles/Flex";
+
+export const HomeContainer = styled(Flex)`
+flex-direction: column;
+`
+export HomeImg = styled.div`
+  width: 35%;
+  max-height: 50vh;
+  border-radius1rem;
+`
+export const MainContainer =styled(Flex)`
+flex-direction: row;;
+`
